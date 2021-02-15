70c7f1e7d5c1f0a5e13a609f73aeb24e3bb7a5a2
valeryshel <valeryshel@gmail.com>
2021-02-15 12:42:35 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+    let minVal = array[0];
+    for (let i = 0; i<array.length; i++) {
+        if(array[i] < minVal){
+            minVal = array[i];
+        }
+    }
+  return minVal;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let maxVal = array[0];
+    for (let i = 0; i<array.length; i++) {
+        if(array[i] > maxVal){
+            maxVal = array[i];
+        }
+    }
+  return maxVal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let average = 0;
+    for (let i = 0; i < array.length; i++ ) {
+        average = (average + array[i])/array.length;
+    }
+  return average;
 }
