e71b4e28d4deec6a990c5b0c77f4179d9405ecf5
valeryshel <valeryshel@gmail.com>
2021-02-15 14:20:07 +0300

    fix: add correct check

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 
 exports.min = function min (array) {
+    if (!array || !array.length) {
+        return 0;
+    }
     let minVal = array[0];
     for (let i = 0; i<array.length; i++) {
         if(array[i] < minVal){
             minVal = array[i];
         }
-        if(array[i] = '') {
-            array[i] = 0;
-        }
+
     }
   return minVal;
 }
 
 exports.max = function max (array) {
+    if (!array || !array.length) {
+        return 0;
+    }
     let maxVal = array[0];
     for (let i = 0; i<array.length; i++) {
         if(array[i] > maxVal){
             maxVal = array[i];
-        }
-        if(array[i] = '') {
-            array[i] = 0
         }
     }
   return maxVal;
 }
 
 exports.avg = function avg (array) {
-    let average = 0;
-    for (let i = 0; i < array.length; i++ ) {
-        if(array[i] = '') {
-            array[i] = 0;
-        }
-        average = (average + array[i])/array.length;
+    if (!array || !array.length) {
+        return 0;
     }
+    let total = 0;
+    for (let i = 0; i < array.length; i++ ) {
+        total = total + array[i];
+    }
+    let average = total/array.length;
   return average;
 }
